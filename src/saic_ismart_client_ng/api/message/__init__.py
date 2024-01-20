3af16889bc76de2678829a99ce96408d8d911e3f
Giovanni Condello <giovanni.condello@coderit.it>
2024-01-20 17:13:19 +0100

    Message handling

--- src/saic_ismart_client_ng/api/message/__init__.py
+++ src/saic_ismart_client_ng/api/message/__init__.py
@@ -1,10 +1,20 @@
+from typing import Optional
+
 from saic_ismart_client_ng.api.base import AbstractSaicApi
 from saic_ismart_client_ng.api.message.schema import MessageResp, UpateMessageRequest
 
 
 class SaicMessageApi(AbstractSaicApi):
+    async def get_alarm_list(self, *, page_num: int, page_size: int) -> MessageResp:
+        return await self.get_message_list(page_num=page_num, page_size=page_size, message_group='ALARM')
 
-    async def get_message_list(self, page_num: int, page_size: int, message_group: str) -> MessageResp:
+    async def get_command_list(self, *, page_num: int, page_size: int) -> MessageResp:
+        return await self.get_message_list(page_num=page_num, page_size=page_size, message_group='COMMAND')
+
+    async def get_news_list(self, *, page_num: int, page_size: int) -> MessageResp:
+        return await self.get_message_list(page_num=page_num, page_size=page_size, message_group='NEWS')
+
+    async def get_message_list(self, *, page_num: int, page_size: int, message_group: str) -> MessageResp:
         return await self.execute_api_call(
             "GET",
             "/message/list",
@@ -15,6 +25,28 @@ class SaicMessageApi(AbstractSaicApi):
             },
             out_type=MessageResp
         )
+
+    async def delete_all_alarms(self):
+        return await self.__change_message_status(action='DELETE_ALARM')
+
+    async def delete_all_commands(self):
+        return await self.__change_message_status(action='DELETE_COMMAND')
+
+    async def delete_all_news(self):
+        return await self.__change_message_status(action='DELETE_NEWS')
+
+    async def read_message(self, *, message_id: int):
+        return await self.__change_message_status(message_id=message_id, action='READ')
+
+    async def delete_message(self, *, message_id: int):
+        return await self.__change_message_status(message_id=message_id, action='DELETE')
+
+    async def __change_message_status(self, *, action: str, message_id: Optional[int] = None):
+        request = UpateMessageRequest(
+            actionType=action,
+            messageId=message_id,
+        )
+        return await self.update_message_status(request)
 
     async def update_message_status(self, data: UpateMessageRequest):
         return await self.execute_api_call(
