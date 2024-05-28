b5dbe9587942cef930ccd2fe6dbd13d12d3d413e
Giovanni Condello <condellog@gmail.com>
2024-05-28 23:34:14 +0200

    Add expected values during encryption tests

--- tests/security_test.py
+++ tests/security_test.py
@@ -15,7 +15,8 @@ class TestGetAppVerificationString(unittest.TestCase):
 
         result = security.get_app_verification_string(clazz_simple_name, request_path, current_ts, tenant_id,
                                                       content_type, request_content, user_token)
-        self.assertIsInstance(result, str)
+
+        self.assertEqual('afd4eaf98af2d964f8ea840fc144ee7bae95dbeeeb251d5e3a01371442f92eeb', result)
 
     def test_with_empty_request_path(self):
         clazz_simple_name = 'SampleClass'
@@ -28,7 +29,7 @@ class TestGetAppVerificationString(unittest.TestCase):
 
         result = security.get_app_verification_string(clazz_simple_name, request_path, current_ts, tenant_id,
                                                       content_type, request_content, user_token)
-        self.assertIsInstance(result, str)
+        self.assertEqual('ff8cb13ebcce5958e7fbfe602716c653fd72ce78842be87b6d50dccede198735', result)
 
     def test_with_no_request_content(self):
         clazz_simple_name = 'SampleClass'
@@ -41,8 +42,7 @@ class TestGetAppVerificationString(unittest.TestCase):
 
         result = security.get_app_verification_string(clazz_simple_name, request_path, current_ts, tenant_id,
                                                       content_type, request_content, user_token)
-        self.assertIsInstance(result, str)
-
+        self.assertEqual('332c85836aa9afc864282436a740eb2cc778fafd1fea74dd887c1f8de5056de0', result)
 
 if __name__ == '__main__':
     unittest.main()
